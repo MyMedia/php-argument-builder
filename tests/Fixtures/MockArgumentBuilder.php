36714dd1c5bf232733815dd106fe325bf1207935
Tomas Vacula <tomas.vacula@feedo.cz>
2019-01-16 15:21:40 +0100

    update tests

--- tests/Fixtures/MockArgumentBuilder.php
+++ tests/Fixtures/MockArgumentBuilder.php
@@ -7,19 +7,19 @@ use Feedo\AbstractArgumentBuilder\AbstractArgumentBuilder;
 /**
  * @method $this getArg1()
  * @method       setArg1($value, $_ = null)
- * @method $this unsetArg1()
+ * @method $this unsetArg1($_ = null)
  * @method       getArg2()
  * @method $this setArg2($value, $_ = null)
- * @method       unsetArg2()
+ * @method       unsetArg2($_ = null)
  * @method $this getSub1()
  * @method       setSub1($value, $_ = null)
- * @method       unsetSub1()
+ * @method       unsetSub1($_ = null)
  * @method $this getSub2()
  * @method       setSub2($value, $_ = null)
- * @method       unsetSub2()
+ * @method       unsetSub2($_ = null)
  * @method $this getEnum()
- * @method       setEnum($value)
- * @method       unsetEnum()
+ * @method       setEnum($value, $_ = null)
+ * @method       unsetEnum($_ = null)
  */
 class MockArgumentBuilder extends AbstractArgumentBuilder
 {
